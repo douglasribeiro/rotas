f54b1fc82bc6528d91baab3e8f0a589a996d1a1e
Douglas Ribeiro <douglas.ribeiro.id@gmail.com>
2020-07-23 11:47:54 -0300

    Rotas Simples (50)

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,21 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { LoginComponent } from './login/login.component';
+import { CursoComponent } from './curso/curso.component';
+import { routing } from './app.routing';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    LoginComponent,
+    CursoComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    routing
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/app.routing.ts
@@ -0,0 +1,15 @@
+import { ModuleWithProviders } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+
+import { HomeComponent } from './home/home.component';
+import { LoginComponent } from './login/login.component';
+import { CursoComponent } from './curso/curso.component';
+
+
+const APP_ROUTES: Routes = [
+  { path: 'curso', component: CursoComponent },
+  { path: 'login', component: LoginComponent },
+  { path: '', component: HomeComponent }
+]
+
+export const routing: ModuleWithProviders = RouterModule.forRoot(APP_ROUTES);
