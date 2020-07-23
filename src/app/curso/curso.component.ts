f7e5f54dbc2e6427e11a3a6bdeb50c4fc86d8ea2
Douglas Ribeiro <douglas.ribeiro.id@gmail.com>
2020-07-23 15:59:17 -0300

    Rotas parametros de url query (56)

--- src/app/curso/curso.component.ts
+++ src/app/curso/curso.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { CursoService } from './curso.service';
+import { ActivatedRoute, Router } from '@angular/router';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-curso',
@@ -9,11 +11,33 @@ import { CursoService } from './curso.service';
 export class CursoComponent implements OnInit {
 
   cursos: any[];
+  pagina: number;
+  inscricao: Subscription;
 
-  constructor(private cursoService: CursoService) { }
+  constructor(
+    private cursoService: CursoService,
+    private route: ActivatedRoute,
+    private router: Router) { }
 
   ngOnInit(): void {
     this.cursos = this.cursoService.getCursos();
+
+    this.inscricao = this.route.queryParams.subscribe(
+      (queryParams: any) => {
+        this.pagina = queryParams['pagina'];
+      }
+    )
+  }
+
+  ngOnDestroy(): void {
+    this.inscricao.unsubscribe();
+  }
+
+  proximaPagina(){
+    //this.pagina++;
+    this.router.navigate(['/curso'], {
+      queryParams: {'pagina': ++this.pagina}
+    })
   }
 
 }
