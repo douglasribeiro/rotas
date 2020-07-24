5ccf651a8ce29609995f05013536b6631b997b73
Douglas Ribeiro <douglas.ribeiro.id@gmail.com>
2020-07-24 19:26:25 -0300

    Rotas filhas desenvolvendo telas (60)

--- src/app/alunos/aluno-form/aluno-form.component.ts
+++ src/app/alunos/aluno-form/aluno-form.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { Subscription } from 'rxjs';
+import { ActivatedRoute } from '@angular/router';
+import { AlunosService } from '../alunos.service';
 
 @Component({
   selector: 'app-aluno-form',
@@ -7,9 +10,30 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AlunoFormComponent implements OnInit {
 
-  constructor() { }
+  aluno: any;
+  inscricao: Subscription;
+
+  constructor(
+    private route: ActivatedRoute,
+    private alunoService: AlunosService
+  ) { }
 
   ngOnInit(): void {
+    this.inscricao = this.route.params.subscribe(
+      (params: any) => {
+        let id = params['id'];
+
+        this.aluno = this.alunoService.getAluno(id);
+
+        if(this.aluno === null){
+          this.aluno = {};
+        }
+      }
+    );
+  }
+
+  ngOnDestroy(): void {
+    this.inscricao.unsubscribe;
   }
 
 }
--- src/app/alunos/alunos.module.ts
+++ src/app/alunos/alunos.module.ts
@@ -1,20 +1,24 @@
 import { NgModule } from "@angular/core";
 import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+
 import { AlunosComponent } from './alunos.component';
 import { AlunoFormComponent } from './aluno-form/aluno-form.component';
 import { AlunoDetalheComponent } from './aluno-detalhe/aluno-detalhe.component';
 import { AlunosRoutingModule } from './alunos.routing.module';
+import { AlunosService } from './alunos.service';
 
 @NgModule({
-  providers: [],
   imports: [
     CommonModule,
+    FormsModule,
     AlunosRoutingModule
   ],
   declarations: [
     AlunosComponent,
     AlunoFormComponent,
     AlunoDetalheComponent
-  ]
+  ],
+  providers: [AlunosService]
 })
 export class AlunosModule{}
